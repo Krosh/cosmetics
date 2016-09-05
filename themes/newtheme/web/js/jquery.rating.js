2f3aebc6ee566f3b67671fe10e1f4356334b292c
u4aew <dialoggg1994@gmail.com>
2016-09-05 13:09:08 +0700

    fixed

--- themes/newtheme/web/js/jquery.rating.js
+++ themes/newtheme/web/js/jquery.rating.js
@@ -89,62 +89,6 @@
             if (this.options.readOnly) return;
 
             var self = this, left = 0, width = 0;
-
-            this.vote_hover.bind('mousemove mouseover', function (e) {
-
-                if (self.options.readOnly) return;
-
-                var $this = $(this),
-                    score = 0;
-
-                left = e.clientX > 0 ? e.clientX : e.pageX;
-                width = left - $this.offset().left - 2;
-
-                var max = self.width * self.options.stars,
-                    min = self.options.minimal * self.width;
-
-                if (width > max) width = max;
-                if (width < min) width = min;
-
-                score = Math.round(width / self.width * 10) / 10; //округляем до 1 знака после запятой
-
-                if (self.options.fx == 'half') {
-                    width = Math.ceil(width / self.width * 2) * self.width / 2;
-                }
-                else if (self.options.fx != 'float') {
-                    width = Math.ceil(width / self.width) * self.width;
-                }
-
-                score = Math.round(width / self.width * 10) / 10;
-
-                self.vote_active.css({
-                    'width': width,
-                    'background-position': 'left center'
-                });
-
-            })
-                .bind('mouseout', function () {
-                    if (self.options.readOnly) return;
-                    self.reset();
-                    self.vote_success.empty();
-                }).bind('click.ratingReviews', function () {
-
-                if (self.options.readOnly) return;
-
-                var score = Math.round(width / self.width * 10) / 10;
-
-                if (score > self.options.stars) score = self.options.stars;
-                if (score < 0) score = 0;
-
-                self.old = self._data.val;
-                self._data.val = (self._data.val * self._data.votes + score) / (self._data.votes + 1);
-                self._data.val = Math.round(self._data.val * 100) / 100;
-                self._data.score = score;
-                $("#modal__ratingReviews").val(score);
-                self.options.readOnly = true;
-                self.options.click.apply(this, [score]);
-            });
-
         },
         set: function () {
             this.vote_active.css({
